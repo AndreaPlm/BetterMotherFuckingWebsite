18b98d2c6e20127536489b8165c122460288cee7
AndreaPlm <palma.a.andrea@gmail.com>
2018-09-12 14:24:19 +0200

    JS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.setTimeout(function () {
+    let userName=window.prompt('Quel est ton nom ?');
+    var confirmation=confirm('Hey ' + userName +' le site xxxvidsxxx est trop bien. Voulez vous le visiter ?');
+        if (confirmation){ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+        else {null}
+}, 10000);
