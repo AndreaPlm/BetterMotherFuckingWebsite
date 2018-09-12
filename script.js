91024c4de9bc57692675e50195af2af9c632696b
AndreaPlm <palma.a.andrea@gmail.com>
2018-09-12 14:56:57 +0200

    Done done done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.setTimeout(function () {
+    let userName=window.prompt('Quel est ton nom ?');
+    let confirmation=confirm('Hey ' + userName +' le site xxxvidsxxx est trop bien. Voulez vous le visiter ?');
+        if (confirmation){ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+        else {confirm('Vous êtes sur ? Cliquez Annuler pour fermer la boite de dialogue')}
+            if (true){document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+            else {close.window() }
+
+}, 10000);
