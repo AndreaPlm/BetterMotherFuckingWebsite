ceb7c6be5b743d41f612d67c9c81ce4e060363e9
AndreaPlm <palma.a.andrea@gmail.com>
2018-09-12 15:30:04 +0200

    debug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 window.setTimeout(function () {
     let userName=window.prompt('Quel est ton nom ?');
+    if (userName===null) {return false}
+
     let confirmation=confirm('Hey ' + userName +' le site xxxvidsxxx est trop bien. Voulez vous le visiter ?');
-        if (confirmation){ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        else {confirm('Vous êtes sur ? Cliquez Annuler pour fermer la boite de dialogue')}
-            if (true){document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-            else {close.window() }
+          if (confirmation){ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+          else {window.close ()};
 
 }, 10000);
