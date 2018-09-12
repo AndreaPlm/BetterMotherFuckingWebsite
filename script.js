e0115c71bd7cae68cd1ea07b6fd52f566137a8dc
AndreaPlm <palma.a.andrea@gmail.com>
2018-09-12 17:15:56 +0200

    Psychédélique !

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-window.setTimeout(function () {
+/*window.setTimeout(function () {
     let userName=window.prompt('Quel est ton nom ?');
     if (userName===null) {return false}
 
@@ -7,4 +7,9 @@ window.setTimeout(function () {
           if (confirmation){ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
           else {window.close ()};
 
-}, 10000);
+}, 10000);*/
+
+
+function switchStyle(style){
+document.getElementById("styleNormal").setAttribute("href", style);
+}
